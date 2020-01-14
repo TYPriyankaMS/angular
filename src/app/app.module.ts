eea75fec7f02c73b36169e83f2361dcb84500186
GiridharaMG <mg.giridhar@gmail.com>
2020-01-14 11:24:36 +0530

    Class Project 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { LocationComponent } from './location/location.component';
+import { AboutHomeComponent } from './about-home/about-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    HeaderComponent,
+    PageNotFoundComponent,
+    ContactusComponent,
+    LocationComponent,
+    AboutHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      {
+        path: 'about', component: AboutComponent, children: [
+          { path: '', component: AboutHomeComponent },
+          { path: 'contact-us', component: ContactusComponent },
+          { path: 'location', component: LocationComponent }
+        ]
+      },
+      { path: '**', component: PageNotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
