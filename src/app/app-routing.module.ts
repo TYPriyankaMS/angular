e2d0566b8b795bc2e8d3b0a59709e0ec73cf4084
TYPriyankaMS <priyanka.ms@testyantra.com>
2020-03-19 11:47:47 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Login1Module } from './login1/login1.module';
+import { Login1Component } from './login1/login1.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login1',component:Login1Component}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+ Login1Module ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
